99d35325a8dec2bc023b72bc656bab0b860d72ef
gvpraagh <83592872+gvpraagh@users.noreply.github.com>
2024-12-04 15:56:42 +0100

    Update background.py

--- Algorithm_clean/background.py
+++ Algorithm_clean/background.py
@@ -42,7 +42,6 @@ def get_background(label):
             radius2 = radius
     slice_range = 12/label.GetSpacing()[2]
     kernel_radius = math.floor(radius2*2-12/label.GetSpacing()[0])
-    plt.imshow(middle_slice_np)
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_radius, kernel_radius))
     background_np = np.zeros(label_np.shape)
     for i in range(math.floor(middle_slice-slice_range/2),math.ceil(middle_slice+slice_range/2)):
@@ -53,50 +52,5 @@ def get_background(label):
     background.SetDirection(label.GetDirection())
     background.SetOrigin(label.GetOrigin())
 
-    # erode = sitk.BinaryErodeImageFilter()
-    # erode.SetKernelType(sitk.sitkBall)
-    # erode.SetKernelRadius(round(radius2-6/label.GetSpacing()[0])) #zodat er 6 mm overblijft
-    # erode.SetForegroundValue(1)
-    # background = erode.Execute(label)
-    # labelShapeFilter = sitk.LabelShapeStatisticsImageFilter()
-    # labelShapeFilter.Execute(background)
-    # Bbox = labelShapeFilter.GetBoundingBox(1)
-    # if Bbox[5] > 6:
-    #     background_np = sitk.GetArrayFromImage(background)
-    #     new_background_np = np.zeros(background_np.shape)
-    #     start_slice = int(Bbox[2] + Bbox[5]/2 - 3)
-    #     end_slice = start_slice + 6
-    #     new_background_np[start_slice:end_slice,:,:] = background_np[start_slice:end_slice,:,:]
-    #     background = sitk.GetImageFromArray(new_background_np)
-    #     background.SetSpacing(label.GetSpacing())
-    #     background.SetDirection(label.GetDirection())
-    #     background.SetOrigin(label.GetOrigin())
-    # elif Bbox[5] < 6:
-    #     contours, _ = cv2.findContours(middle_slice_np,2,1)
-    #     for i in range(len(contours)):
-    #         (x, y), radius = cv2.minEnclosingCircle(contours[i])
-    #         if i == 0:
-    #             radius2 = radius
-    #         elif radius < radius2:
-    #             radius2 = radius
-    #     print(radius2)
-    #     erode = sitk.BinaryErodeImageFilter()
-    #     erode.SetKernelType(sitk.sitkBall)
-    #     erode.SetKernelRadius(round(radius2-6*label.GetSpacing()[0])) #zodat er 6 mm overblijft
-    #     erode.SetForegroundValue(1)
-    #     background = erode.Execute(label)
-    #     labelShapeFilter = sitk.LabelShapeStatisticsImageFilter()
-    #     labelShapeFilter.Execute(background)
-    #     Bbox = labelShapeFilter.GetBoundingBox(1)
-    #     if Bbox[5] > 6:
-    #         background_np = sitk.GetArrayFromImage(background)
-    #         new_background_np = np.zeros(background_np.shape)
-    #         start_slice = int(Bbox[2] + Bbox[5]/2 - 3)
-    #         end_slice = start_slice + 6
-    #         new_background_np[start_slice:end_slice,:,:] = background_np[start_slice:end_slice,:,:]
-    #         background = sitk.GetImageFromArray(new_background_np)
-    #         background.SetSpacing(label.GetSpacing())
-    #         background.SetDirection(label.GetDirection())
-    #         background.SetOrigin(label.GetOrigin())
-    
     return background
+
